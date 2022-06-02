dc39d6bcbfb1f29c861323a0ae8991ff1589e114
DyPeterson <dylan.peterson17@gmail.com>
2022-06-02 15:29:01 -0700

    [WIP] Colorizing each section of print function

--- c5e2/ex1/main.py
+++ c5e2/ex1/main.py
@@ -4,11 +4,14 @@ import colored as cld
 def print_yml(yaml_path):
     with open(yaml_path) as open_yaml:
         docs = yaml.full_load(open_yaml)
-        for key, val in docs.items():
-            color_key = ((cld.fg(14) + cld.bg(17) + cld.attr(4)) + key + cld.attr(0))
-            # color_val = () + val + cld.attr(0)
-            print(color_key, val)
-
+        ingredients = docs['ingredients']
+        instructions = docs['instructions']
+        notes = docs['notes']
+        nutrition = docs['nutrition']
+        # for key, val in nutrition.items():
+        #     color_key = ((cld.fg(14) + cld.bg(17) + cld.attr(4)) + key + cld.attr(0))
+        #     print(color_key, color_key2)
+        print((notes + cld.fg(30)))
 def run():
     print_yml('../data/potato_salad.yml')
 
