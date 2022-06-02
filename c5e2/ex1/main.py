d829b7cde0c32cc6bb6cafa77f4c343bb87d2c4a
DyPeterson <dylan.peterson17@gmail.com>
2022-06-02 14:56:10 -0700

    add color to key output

--- c5e2/ex1/main.py
+++ c5e2/ex1/main.py
@@ -1,10 +1,13 @@
 import yaml
+import colored as cld
 
 def print_yml(yaml_path):
     with open(yaml_path) as open_yaml:
         docs = yaml.full_load(open_yaml)
         for key, val in docs.items():
-            print(key, val)
+            color_key = ((cld.fg(14) + cld.bg(17) + cld.attr(4)) + key + cld.attr(0))
+            # color_val = () + val + cld.attr(0)
+            print(color_key, val)
 
 def run():
     print_yml('../data/potato_salad.yml')
